05b63618edc61eecc09d3d9b97c5a2a91287af64
悬剑 <xiangjian.xj@alibaba-inc.com>
2021-07-26 09:02:50 +0800

    职责链

---
+++ src/ChainOfResponsibilityPattern/orderChainDemo.js
@@ -0,0 +1,50 @@
+
+const order500=(value)=>{
+  if(value===500){
+    console.log('500--->')
+  }else{
+    return 'next'
+  }
+}
+
+
+const order200=(value)=>{
+  if(value===200){
+    console.log('200--->')
+  }else{
+    return 'next'
+  }
+}
+
+const order=(value)=>{
+  if(value!==200||value!==500){
+    console.log('normal--->')
+  }else{
+    console.log('没有命中')
+  }
+}
+
+const Chain=function(fn){
+  this.fn=fn;
+  this.successor=null;
+}
+Chain.prototype.setSuccessor=function(successor){
+  return this.successor=successor;
+}
+Chain.prototype.passRequest=function(){
+  let ret=this.fn.apply(this,arguments);
+  if(ret==='next'){
+  return this.successor&&this.successor.passRequest.apply(this.successor,arguments);
+
+  }
+  return ret;
+
+}
+
+let chainOrder500=new Chain(order500);
+let chainOrder200=new Chain(order200);
+let chainOrder=new Chain(order);
+
+chainOrder500.setSuccessor(chainOrder200);
+chainOrder200.setSuccessor(chainOrder);
+chainOrder500.passRequest(4)
